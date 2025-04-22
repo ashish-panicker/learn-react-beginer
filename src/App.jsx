370fdd3510c5f7a2058036c4c1459e63c3e5ce05
ashish-panicker <ashish.s.panicker@proton.me>
2025-04-22 11:08:17 +0530

    Working with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import { sculptureList } from './data'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [index, setIndex] = React.useState(0)
+  let sculpture = sculptureList[index]
+
+  function next() {
+    let newindex = 0
+    if (index === sculptureList.length - 1) {
+      newindex = 0
+    } else {
+      newindex = index + 1
+    }
+    setIndex(newindex)
+  }
+  function previous() { }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Sculptures</h1>
+      <button onClick={next}>Next</button>
+      <span>{index + 1} of {sculptureList.length}</span>
+      <button>Previous</button>
+      <h2> <i>{sculpture.name} </i> by {sculpture.artist}</h2>
+      <p>{sculpture.description}</p>
     </>
   )
 }
